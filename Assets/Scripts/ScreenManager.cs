b75495144f94cdb9b0bd16f5b84329da2f8f719c
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2017-03-14 16:37:22 +0000

    Resize an overflowing window to 2/3rds of the screen

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -8,11 +8,20 @@ public class ScreenManager : MonoBehaviour {
 
     private void SetResolution() {
 #if !UNITY_EDITOR
-        Screen.SetResolution(
-            (int) Mathf.Round(Screen.currentResolution.height * 9f / 16f),
-            Screen.currentResolution.height,
-            false
+
+        int newWidth = (int) Mathf.Round(
+            Screen.currentResolution.height * 9f / 16f
         );
+
+        // If the window is too tall for the screen.
+        if (Screen.currentResolution.width != newWidth) {
+            int newHeight = Screen.currentResolution.height * 2 / 3;
+
+            newWidth = (int) Mathf.Round(newHeight * 9f / 16f);
+
+            Screen.SetResolution(newWidth, newHeight, false);
+        }
+
 #endif
     }
 
